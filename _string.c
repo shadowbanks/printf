11b4b802b5cd5d4770c8f6e0a4fa6b718389cbb4
Ebunoluwa oke <ebunoke18@gmail.com>
2022-10-18 20:39:25 -0700

    new changes

--- _string.c
+++ _string.c
@@ -1,5 +1,11 @@
-#include <unistd.h>
+#include <unistd.h7>
 
+/**
+ * _string - prints a string
+ * @str: the string to be printed
+ * @n: the length of the string
+ * Return: string
+ */
 
 int _string(char *str, int n)
 {
